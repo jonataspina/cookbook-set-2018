d589aa9d377259e692cb2e77a3ce0354d808ec68
Jonatas e Willian <jonatas_pina@hotmail.com>
2018-09-17 20:56:14 -0300

    Usuário envia foto de uma receita

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,10 @@ class Recipe < ApplicationRecord
   belongs_to :cuisine
 
   validates :title, :cuisine, :difficulty, :cook_time,
-            :ingredients, :cook_method, presence: true
+            :ingredients, :cook_method,  presence: true
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: 'image/jpeg', message: 'Tipo da imagem não é válido'
 
   def cook_time_min
     "#{cook_time} minutos"
